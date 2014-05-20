f2c4745444fd4364476d43cd4f1dace9b3113a98
seldoot <tl.vandoren@gmail.com>
2014-05-19 21:11:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShrubSteppe::Application.config.secret_key_base = '27c36c9364fe23c6eee0e265ab37449642c0daef637d6fb92de13515d90766f97d6f44bdb4b74f41852cd89ec6340bcfe029e72a5e0780c34b0cf944391c1d87'
